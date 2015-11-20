0c14cb81ad68db344fef79e0996979c861883aeb
matthewpalmer <matt@matthewpalmer.net>
2015-11-21 07:08:24 +1100

    Initial Commit

---
+++ MPNavigationController/MPNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  MPNavigationController.h
+//  MPNavigationController
+//
+//  Created by Matthew Palmer on 21/11/2015.
+//  Copyright © 2015 Matthew Palmer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MPNavigationController.
+FOUNDATION_EXPORT double MPNavigationControllerVersionNumber;
+
+//! Project version string for MPNavigationController.
+FOUNDATION_EXPORT const unsigned char MPNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPNavigationController/PublicHeader.h>
+
+
